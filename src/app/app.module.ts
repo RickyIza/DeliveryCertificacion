a74a202c607ca29879143810c92bede7d4014dad
Ricky Iza <rck.ikbm@gmail.com>
2020-07-22 19:41:09 -0500

    Form Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { ProductoMainComponent } from './components/producto-main/producto-main.component';
 import { ProductoFormComponent } from './components/producto-main/producto-form/producto-form.component';
 import { ProductoListComponent } from './components/producto-main/producto-list/producto-list.component';
